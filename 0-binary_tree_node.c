5c0c3e89bb2ece6ab8102431479b0713eee6a4f7
Daniel Berthran <berthlearn@gmail.com>
2023-06-01 10:51:36 +0100

    How to create a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+
+/**
+* binary_tree_node - creates a binary tree node
+* @parent: Pointer to the parent node of the created node
+* @value: Value to store in new node
+* Return: Pointer to new node or NULL on failure
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (!node)
+		return (NULL);
+
+	node->n = value;
+
+	if (parent == NULL)
+	{
+		/* Create a root node */
+		node->parent = NULL;
+	}
+	else
+	{
+		/* Create a leaf node */
+		node->parent = parent;
+	}
+	return (node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -14,10 +14,10 @@
 
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -33,4 +33,5 @@ typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+void binary_tree_print(const binary_tree_t *tree);
 #endif
